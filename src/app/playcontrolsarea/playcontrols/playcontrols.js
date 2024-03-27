679d9c4983f9deac4d225b2d5e1653436e774b86
Daniel Novikov <dnovikov@optonline.net>
2024-03-27 01:09:47 -0400

    buttons react to being pushed

--- src/app/playcontrolsarea/playcontrols/playcontrols.js
+++ src/app/playcontrolsarea/playcontrols/playcontrols.js
@@ -16,13 +16,13 @@ export default function PlayControls({isPlaying, setIsPlaying, setPlayheadPositi
     <div className={styles.playcontrols}>
 
       {/* jump to start */}
-      <div  className={styles.controlbutton} onClick={jumpToStart}>
+      <div className={styles.controlbutton} onClick={jumpToStart}>
         <svg id={styles.rr} fill="none" stroke="#A0A2A4" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
           <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5"></path>
         </svg>
       </div>
 
-      <div  className={styles.controlbutton} onClick={togglePlay}>
+      <div className={`${styles.controlbutton} ${isPlaying ? styles.pushed : ''}`} onClick={togglePlay}>
         {/* play */}
         <svg id={styles.pp} fill="#A0A2A4" stroke="#888888" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
           <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"></path>
